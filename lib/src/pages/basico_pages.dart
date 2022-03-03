6a2f880075c2075d106f840951e0e528d7b13e1d
Romina Uicab <rominauicab1@gmail.com>
2022-03-02 19:37:22 -0500

    Commit Basico Page Parte3

--- lib/src/pages/basico_pages.dart
+++ lib/src/pages/basico_pages.dart
@@ -7,47 +7,56 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          _crearImagen(),
-          _crearTitulo(),
-          _crearAcciones(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
       ),
     );
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          'https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Montañas', style: estiloTitulo),
-                SizedBox(height: 7.0),
-                Text('Casas en una Montaña', style: estiloSubTitulo),
-              ],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Montañas', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('Casas en una Montaña', style: estiloSubTitulo),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0),
-          Text('41', style: TextStyle(fontSize: 20.0))
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
       ),
     );
   }
@@ -74,11 +83,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Pariatur enim sit mollit veniam velit nostrud laboris do reprehenderit anim occaecat.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Pariatur enim sit mollit veniam velit nostrud laboris do reprehenderit anim occaecat. Ea excepteur ea velit ipsum veniam nostrud voluptate incididunt irure minim labore eiusmod. In dolore pariatur quis laborum ad deserunt ipsum eu mollit officia sunt. Aliqua aute exercitation et duis commodo culpa ex ullamco enim reprehenderit cillum ipsum aute. Eiusmod ullamco aliquip eiusmod magna magna magna ut.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
