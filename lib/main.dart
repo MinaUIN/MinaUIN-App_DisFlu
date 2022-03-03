8c6ca9365689cf6d90f1d68f51f7949821db2a80
Romina Uicab <rominauicab1@gmail.com>
2022-03-03 00:23:58 -0500

    Commit Status Bar Títulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:disenos/src/pages/basico_pages.dart';
 import 'package:disenos/src/pages/scroll_pages.dart';
+import 'package:disenos/src/pages/botones_pages.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Color.fromARGB(0, 255, 255, 255)));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
