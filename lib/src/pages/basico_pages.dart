c5cc64c0f8122d4c3b383f38bd55a68cf282507c
Romina Uicab <rominauicab1@gmail.com>
2022-03-02 19:19:42 -0500

    Commit Basico Page Parte2

--- lib/src/pages/basico_pages.dart
+++ lib/src/pages/basico_pages.dart
@@ -9,30 +9,76 @@ class BasicoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(
-            image: NetworkImage(
-                'https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg'),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg'),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Montañas', style: estiloTitulo),
-                      SizedBox(height: 7.0),
-                      Text('Casas en una Montaña', style: estiloSubTitulo),
-                    ],
-                  ),
-                ),
-                Icon(Icons.star, color: Colors.red, size: 30.0),
-                Text('41', style: TextStyle(fontSize: 20.0))
+                Text('Montañas', style: estiloTitulo),
+                SizedBox(height: 7.0),
+                Text('Casas en una Montaña', style: estiloSubTitulo),
               ],
             ),
-          )
+          ),
+          Icon(Icons.star, color: Colors.red, size: 30.0),
+          Text('41', style: TextStyle(fontSize: 20.0))
         ],
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, color: Colors.blue, size: 40.0),
+        SizedBox(height: 5.0),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue)),
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Pariatur enim sit mollit veniam velit nostrud laboris do reprehenderit anim occaecat.',
+        textAlign: TextAlign.justify,
       ),
     );
   }
